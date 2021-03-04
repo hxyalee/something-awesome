a53e2910c4b6bb74fc80589a5648b016a5f9861c
hoya <hoya.lee0@gmail.com>
2021-03-04 23:42:23 +1100

    Initial commit

---
+++ background.js
@@ -0,0 +1,10 @@
+chrome.tabs.onActivated.addListener((tab) => {
+  chrome.tabs.get(tab.tabId, (currentTab) => {
+    if (/^https:\/\/www/.test(currentTab.url)) {
+      chrome.tabs.executeScript(null, { file: "./foreground.js" }, () => {
+        chrome.tabs.insertCSS(null, { file: "./styles.css" });
+        console.log("foreground running");
+      });
+    }
+  });
+});
---
+++ main.js
@@ -0,0 +1,18 @@
+function readTextFile(file, callback) {
+    let rawFile = new XMLHttpRequest();
+    rawFile.overrideMimeType("application/json");
+    rawFile.open("GET", file, true);
+    rawFile.onreadystatechange = function() {
+        if (rawFile.readyState === 4 && rawFile.status == "200") {
+            callback(rawFile.responseText);
+        }
+    }
+    rawFile.send(null);
+}
+
+readTextFile("secrets.json", function(text){
+    let data = JSON.parse(text);
+    console.log(data);
+});
+
+console.log('here')
